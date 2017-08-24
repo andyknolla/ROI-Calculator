0938a1a445fd68c46feb87abb8358be828234f8d
Andy Knolla <andyknolla@gmail.com>
2017-08-23 18:43:33 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>ROI Calculator</h2>
+        </div>
+        <p className="App-intro"></p>
+      </div>
+    );
+  }
+}
+
+export default App;
